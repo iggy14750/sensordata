9d6cdc0ef67b6cfa032daea0b975d2eef5f884c9
Isaac <isaacgoss14750@gmail.com>
2017-12-02 01:48:45 -0500

    rolling my own dynamic array

--- src/main/java/capture/SensorData.java
+++ src/main/java/capture/SensorData.java
@@ -1,29 +1,53 @@
 
 package capture;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class SensorData {
 
-    private List<Double> gx;
+    private static final int DIMENSIONS = 3;
+    private static final int DEFAULT_SIZE = 8;
+
+    private int size;
+    private double[][] gyro;
 
     public SensorData() {
-        gx = new ArrayList();
+        size = 0;
+        gyro = new double[DIMENSIONS][DEFAULT_SIZE];
     }
 
     public void insert(int timestamp, 
         double gx, double gy, double gz,
         double ax, double ay, double az
     ) {
-        this.gx.add(gx);
+        ensureCapacity();
+        gyro[0][size] = gx;
+        gyro[1][size] = gy;
+        gyro[2][size] = gz;
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void ensureCapacity() {
+        if (gyro[0].length == size) {
+            for (int i = 0; i < DIMENSIONS; i++) {
+                int n = gyro[i].length;
+                double[] temp = new double[2*n];
+                for (int j = 0; j < n; j++) {
+                    temp[j] = gyro[i][j];
+                }
+                gyro[i] = temp;
+            }
+        }
     }
 
     public double[] getGx() {
-        double[] res = new double[gx.size()];
-        for (int i = 0; i < gx.size(); i++) {
-            res[i] = (double) gx.get(i);
-        }
-        return res;
+        return gyro[0];
+    }
+
+    public double[] getGy() {
+        return gyro[1];
     }
 }
--- src/test/java/capture/TestSensorData.java
+++ src/test/java/capture/TestSensorData.java
@@ -3,22 +3,33 @@ package capture;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Arrays;
 
 public class TestSensorData {
 
+    private final double epsilon = 0.1;
     private final double[] testData = new double[] {
-        42.0, 2.23, 234.23, -23.32, 23.42, -63.2, 49.01, -57
+        42.0, 2.23, 234.23, -23.32, 23.42, -63.2, 49.01, -57, 23
     };
 
-    private final double epsilon = 0.1;
+    SensorData sd = new SensorData();
 
     @Test
     public void gx() {
-        SensorData sd = new SensorData();
         for (double x: testData) {
             sd.insert(0,x,0,0,0,0,0);
         }
-        assertArrayEquals(testData, sd.getGx(), epsilon);
+        assertEquals(9, sd.size());
+        assertArrayEquals(testData, Arrays.copyOf(sd.getGx(), sd.size()), epsilon);
+    }
+
+    @Test
+    public void gy() {
+        for (double x: testData) {
+            sd.insert(0,0,x,0,0,0,0);
+        }
+        assertEquals(9, sd.size());
+        assertArrayEquals(testData, Arrays.copyOf(sd.getGy(), sd.size()), epsilon);
     }
 
 }
